2e59257a06219d9607907193503d7071d8af2197
OL EEVIN <138324008+oleevin@users.noreply.github.com>
2023-11-17 04:08:29 +0700

    Add files via upload

---
+++ assets/js/music-list.js
@@ -0,0 +1,57 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste @ac it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: " Way Down We Go", //music name
+    artist: "MW2 GHOST", //artist name
+    img: "music-1", //image name
+    src: "music-1"  //music name here
+  },
+  {
+    name: "ustavo Santaolalla",
+    artist: "Babel",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Thank you",
+    artist: "Dido",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Let her go",
+    artist: "Passenger ",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "The Nights",
+    artist: "Avicii",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "fallen kingdom",
+    artist: " Shalom Margaret",
+    img: "music-6",
+    src: "music-6"
+  },
+
+  {
+    name: "Get You The Moon",
+    artist: "Kina",
+    img: "music-7",
+    src: "music-7"
+  },
+
+];
